8cbc7a1266ddab7f99ffc33458782183ba8f070a
alliejj99 <118407356+alliejj99@users.noreply.github.com>
2023-01-10 18:42:14 +0900

    Compare

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-const basketStarterEl = document.querySelector("header .basket-starter");
-const basketEl = basketStarterEl.querySelector(".basket");
+// import = module 이므로,
+// 사용하려면 최상단 html에 type="module"을 추가해야한다.
+// <script defer type="module" src="./js/main.js"></script>
+import ipads from "../data/ipads.js";
 
 /* Drop Down Menu */
+const basketStarterEl = document.querySelector("header .basket-starter");
+const basketEl = basketStarterEl.querySelector(".basket");
 
 function showBasket() {
   basketEl.classList.add("show");
@@ -114,4 +118,32 @@ pauseBtn.addEventListener("click", () => {
   video.pause();
   playBtn.classList.remove("hide");
   pauseBtn.classList.add("hide");
+});
+
+/* JS import하여 내용 출력하기 & Comment tagged templates 확장 프로그램 */
+const itemsEl = document.querySelector("section.compare .items");
+ipads.forEach((ipad) => {
+  const itemEl = document.createElement("div");
+  itemEl.classList.add("item");
+
+  let colorList = "";
+  ipad.colors.forEach((color) => {
+    colorList += `<li style="background-color:${color};"></li>`;
+  });
+
+  itemEl.innerHTML = /* html */ `
+    <div class="thumbnail"> 
+      <img src="${ipad.thumbnail}" alt="${ipad.name}" />
+    </div>
+    <ul class="colors">
+      ${colorList}
+    </ul>
+    <h3 class="name">${ipad.name}</h3>
+    <p class="tagline">${ipad.tagline}</p>
+    <p class="price">₩${ipad.price.toLocaleString("en-US")}부터</p>
+    <button class="btn">구입하기</button>
+    <a href="${ipad.url}" class="link">더 알아보기</a>
+`;
+
+  itemsEl.append(itemEl);
 });
